fb0e38d741a58a693d13a2a31ad3c952c1374c5a
eduardoDeAAMelo <eduardo.melo0802@gmail.com>
2025-05-19 15:28:36 -0300

    Inicio projeto Spring

---
+++ src/main/java/br/com/alura/novoscreenmatch/NovoscreenmatchApplication.java
@@ -0,0 +1,29 @@
+package br.com.alura.novoscreenmatch;
+
+import br.com.alura.novoscreenmatch.model.DadosSerie;
+import br.com.alura.novoscreenmatch.service.ConsumoAPI;
+import br.com.alura.novoscreenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoscreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoscreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=7cd6eaf5");
+	//	System.out.println(json);
+	//	json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+		
+	}
+}
---
+++ src/test/java/br/com/alura/novoscreenmatch/NovoscreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.novoscreenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoscreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
